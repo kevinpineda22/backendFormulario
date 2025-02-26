11e760fb86e2d094e7ecf1bcd69708c8fb2ca8c9
Juan Manuel <juanmerkahorro@gmail.com>
2025-02-26 15:52:41 -0500

    awooooooooooo

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY; // Esta clave es sensible y solo se usa en el backend
+const supabaseServiceKey = process.env.SUPABASE_KEY; // Esta clave es sensible y solo se usa en el backend
 
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
