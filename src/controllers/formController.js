dd9bbcddfd9d3f56687fbe00b635bc42e425e3e3
Juan Manuel <juanmerkahorro@gmail.com>
2025-02-27 17:15:35 -0500

    endpoint para traer historial

--- src/controllers/formController.js
+++ src/controllers/formController.js
@@ -21,3 +21,20 @@ export async function saveFormData(req, res) {
     return res.status(500).json({ error: err.message });
   }
 }
+
+export async function listFormData(req, res) {
+  try {
+    const { data, error } = await supabase
+      .from("sociodemografico")
+      .select("*");
+
+    if (error) {
+      console.error("Error al obtener datos:", error);
+      return res.status(500).json({ error: error.message });
+    }
+    return res.status(200).json(data);
+  } catch (err) {
+    console.error("Error en listFormData:", err);
+    return res.status(500).json({ error: err.message });
+  }
+}
--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { saveFormData } from "../controllers/formController.js";
+import { saveFormData, listFormData  } from "../controllers/formController.js";
 
 const router = express.Router();
 
 // Define la ruta POST para guardar los datos
 router.post("/save", saveFormData);
+
+router.get('/api/form/list', listFormData);
 
 export default router;
